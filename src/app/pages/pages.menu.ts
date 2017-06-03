8f977849b3e4f96c832968100dccfbaf058f1081
samverneck <nodezeirojs@gmail.com>
2017-06-03 00:25:00 -0300

    refatorado talvez!

---
+++ src/app/pages/pages.menu.ts
@@ -0,0 +1,49 @@
+export const PAGES_MENU = [
+  {
+    path: 'pages',
+    children: [
+      {
+        path: 'dashboard',
+        data: {
+          menu: {
+            title: 'general.menu.dashboard',
+            icon: 'ion-android-home',
+            selected: false,
+            expanded: false,
+            order: 0
+          }
+        }
+      },
+      {
+        path: '',
+        data: {
+          menu: {
+            title: 'general.menu.pages',
+            icon: 'ion-document',
+            selected: false,
+            expanded: false,
+            order: 650,
+          }
+        },
+        children: [
+          {
+            path: ['/login'],
+            data: {
+              menu: {
+                title: 'general.menu.login'
+              }
+            }
+          },
+          {
+            path: ['/register'],
+            data: {
+              menu: {
+                title: 'general.menu.register'
+              }
+            }
+          }
+        ]
+      },
+    ]
+  }
+];
